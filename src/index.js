2a369e03df89be1e6fb7074c3068aefc8af9ddb0
Vladimir Ermolin <vladimir.ermolin@fastdev.se>
2020-02-25 01:16:52 +0400

    init: human-readable-number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function toReadable (number) {
-  
-}
+    let simpleName = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+    ];
+
+    let dozen = [
+        'zero',
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety',
+        'hundred'
+    ];
+
+    let strNumber = [];
+    if (Math.floor(number / 100)) {
+        strNumber.push(simpleName[Math.floor(number / 100)]);
+        strNumber.push(dozen[10]);
+    }
+
+    if (number % 100 > 19) {
+        strNumber.push(dozen[Math.floor(number % 100 / 10)]);
+    }
+
+    if (number % 100 < 20 && number % 100 > 0) {
+        strNumber.push(simpleName[number % 100]);
+    } else if (number % 100 % 10 || number === 0) {
+        strNumber.push(simpleName[number % 100 % 10]);
+    }
+
+    return strNumber.join(' ');
+};
